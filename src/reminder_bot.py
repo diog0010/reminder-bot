60e01302c09c025c5961a28c1a7394094ac75bbb
Grant Dioguardi <diog0010@algonquinlive.com>
2023-05-18 17:19:16 -0400

    Enable logging for the bot

--- src/reminder_bot.py
+++ src/reminder_bot.py
@@ -1,4 +1,4 @@
-import os
+import os, logging
 from dotenv import load_dotenv
 from telegram import Updates
 from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ContextTypes, filters
@@ -6,6 +6,12 @@ from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, Con
 # Get bot authentication token from environment
 load_dotenv()
 token = os.environ.get('AUTH_TOKEN')
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
 
 def main():
     return 0
