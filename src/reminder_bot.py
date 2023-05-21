2846e28addbf1954d190741a5b13bca52991551a
Grant Dioguardi <diog0010@algonquinlive.com>
2023-05-21 14:02:56 -0400

    Add start command functionality

--- src/reminder_bot.py
+++ src/reminder_bot.py
@@ -13,10 +13,26 @@ logging.basicConfig(
     level=logging.INFO
 )
 
+async def start(update: Update, context: ContextTypes) -> None:
+    """Display a startup message."""
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Hi there, <b>{update.message.from_user.username}</b>.\n\n" +
+              "I can help stay on top of things by logging your tasks and periodically sending you reminders about them.\n\n" +
+              "Use /help to see a list of commands.",
+        parse_mode='HTML'
+    )
+
 def main() -> None:
     """Start the bot."""
     # Create the application and pass it the authentication token
     application = ApplicationBuilder().token(token).build()
+
+    # Intialize handlers
+    start_handler = CommandHandler('start', start)
+
+    # Register application handlers
+    application.add_handler(start_handler)
 
     # Run the bot until user presses Ctrl+C
     application.run_polling()
