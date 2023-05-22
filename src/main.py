4072c482c8b515c01a3be8c5f0939a03f7c3aeda
Grant Dioguardi <diog0010@algonquinlive.com>
2023-05-21 21:55:45 -0400

    Add conversation handler for new reminders

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os, logging, bot
 from dotenv import load_dotenv
-from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, CommandHandler, ConversationHandler, filters
 
 # Get bot authentication token from environment
 load_dotenv()
@@ -21,9 +21,19 @@ def main() -> None:
     start_handler = CommandHandler('start', bot.start)
     help_handler = CommandHandler('help', bot.help)
 
+    # Initialize conversation handlers
+    new_reminder_handler = ConversationHandler(
+        entry_points=[CommandHandler('new', bot.new_reminder)],
+        states={
+            bot.TITLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.title)]
+        },
+        fallbacks=[CommandHandler('cancel', bot.cancel)]
+    )
+
     # Register application handlers
     application.add_handler(start_handler)
     application.add_handler(help_handler)
+    application.add_handler(new_reminder_handler)
 
     # Notify users of invalid command entry
     unknown_handler = MessageHandler(filters.COMMAND, bot.unknown)
