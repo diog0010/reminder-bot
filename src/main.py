79b8c8d064761d6096accc31ac7a163348fb0ee1
Grant Dioguardi <diog0010@algonquinlive.com>
2023-05-25 03:33:47 -0400

    Adjust the end date regex

--- src/main.py
+++ src/main.py
@@ -38,7 +38,7 @@ def main() -> None:
                                                                '^([0-5][0-9])|'
                                                                '^([0-9])'), bot.custom_interval)],
             bot.START: [MessageHandler(filters.Regex('^(([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'), bot.start_time)],
-            bot.END: [MessageHandler(filters.Regex('^(([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'), bot.end_time)],
+            bot.END: [MessageHandler(filters.Regex('^([0-9][0-9][0-9][0-9]-(0[0-9])|(1[0-2])-([0-2][0-9])|(3[0-1]) ([0-1][0-9])|(2[0-3]):[0-5][0-9]:[0-5][0-9])'), bot.end_time)],
             bot.CONFIRM: [CommandHandler('confirm', bot.confirm)]
         },
         fallbacks=[CommandHandler('cancel', bot.cancel)]
