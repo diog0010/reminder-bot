df4163c613d021229eb08d67c5de401fff458d81
Grant Dioguardi <diog0010@algonquinlive.com>
2023-05-24 22:00:56 -0400

    Fix unbalanced parentheses

--- src/main.py
+++ src/main.py
@@ -29,8 +29,8 @@ def main() -> None:
             bot.TASK: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.task)],
             bot.NOTES: [MessageHandler(filters.TEXT & ~filters.COMMAND, bot.notes), CommandHandler('skip', bot.skip_notes)],
             bot.INTERVAL: [CallbackQueryHandler(bot.interval)],
-            bot.START: [MessageHandler(filters.Regex('^(([0-1][0-9]|2[0-3])):[0-5][0-9]:[0-5][0-9])'), bot.start)],
-            bot.END: [MessageHandler(filters.Regex('^(([0-1][0-9]|2[0-3])):[0-5][0-9]:[0-5][0-9])'), bot.end)],
+            bot.START: [MessageHandler(filters.Regex('^(([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'), bot.start)],
+            bot.END: [MessageHandler(filters.Regex('^(([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'), bot.end)],
             bot.CONFIRM: [CommandHandler('confirm', bot.confirm)]
         },
         fallbacks=[CommandHandler('cancel', bot.cancel)]
